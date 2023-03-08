93d203e9d077a9459520ad0bfec87d9923cfac7e
“Jennifer <jenniferkaiser.tech@gmail.com>
2023-03-08 14:12:02 -0500

    added api functionality and sample call image

--- lottery.py
+++ lottery.py
@@ -1,1 +1,24 @@
 from random import choices
+import sys
+sys.path.append('api.py')
+from api import api_token
+
+"""
+API connection code provided by:
+https://collectapi.com/api/chancegame/lottery-api
+"""
+# import http.client
+
+# conn = http.client.HTTPSConnection("api.collectapi.com")
+
+# headers = {
+#     'content-type': "application/json",
+#     'authorization': api_token
+# }
+
+# conn.request("GET", "/chancegame/usaPowerball", headers=headers)
+
+# res = conn.getresponse()
+# data = res.read()
+
+# print(data.decode("utf-8"))
